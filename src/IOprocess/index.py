0e1b0515e501c3893d65695b382bfd9be99edd04
Brian Chul <brianchul@precoo.co>
2018-06-06 16:28:23 +0800

    update

--- src/IOprocess/index.py
+++ src/IOprocess/index.py
@@ -1,7 +1,17 @@
 import importlib
+from adviseInput import adviseInput
+from calculateQuantify import calculateQuantifyData
+from clearScreen import clearScreen
+#from docx import convertHtmlToDoc
+from inputTitle import inputData
+from keyboardInput import rawInput
+from printScoreData import printData
+from readFile import readFile
+from writeToHTML import writeDataToHtml
+
 
 moduleName = ["adviseInput", "calculateQuantify", "clearScreen",
     "docx", "inputTitle", "keyboardInput", "printScoreData", "readFile", "writeToHTML"]
-for name in moduleName:
-    importlib.import_module(name + ".py")
 
+
+print(rawInput())
