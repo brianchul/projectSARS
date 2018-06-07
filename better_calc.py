124d83caadfc647731195834b709b9360a18a1f5
unknown <404411711@gms.tku.edu.tw>
2018-06-07 15:49:58 +0800

    runnable

--- better_calc.py
+++ better_calc.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
 import time
-#import win32com.client
+
+import pdfkit
 from datetime import datetime
 from pylab import figure, axes, pie, title, savefig
-from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt
 import numpy as np
-import pdfkit
 
 
 grade = [0 for n in range(4)]
@@ -25,9 +24,11 @@ question = [grade,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10]
 rollBack = []
 totalCopies = 0
 adviseList = []
-addAdviseHeader = ["<p><strong>","</strong><strong></strong></p>\n"]
+addAdviseHeader = ["<p><strong>","</strong></p>\n"]
 docTitle = ""
-classData = ["",""]
+classData = ["", "", ""] #date, teacherName, classroom
+hasQualityAdvise = False
+qualityAdvise = ""
 
 
 def rawInput():
@@ -38,7 +39,7 @@ def rollBackData(oldData):
 	oldData = oldData.split(' ')
 	if oldData[len(oldData)-1] is "":
 		oldData.remove("")
-	oldData = map(int,oldData)
+	oldData = list(map(int,oldData))
 	for num2 in range(0,len(oldData)):
 		question[num2][oldData[num2]-1] = question[num2][oldData[num2]-1]-1
 
@@ -70,13 +71,15 @@ def readFile(readHtmlFileName):
 	f.close()
 	return content
 	
-def writeDataToHtml(writeHtmlFileName,data):
+def writeDataToHtml(writeHtmlFileName, data):
 	thefile = open(writeHtmlFileName, 'w',encoding='utf-8-sig')
 	for item in data:
-		print>>thefile, item
+		thefile.write(item + '\n')
+		#print (thefile, item)
 	thefile.close()
 
-def convertHtmlToDoc(htmlName,docName):
+'''
+def convertHtmlToDoc(htmlName, docName):
 	docName = "output/" + docName + ".doc"
 	word = win32com.client.Dispatch("Word.Application")
 
@@ -96,7 +99,17 @@ def convertHtmlToDoc(htmlName,docName):
 	doc.Close()
 	
 	word.Quit()
-	
+'''
+
+def convertHtmlToPDF(htmlPath, pdfName):
+	pdfkit.from_file(htmlPath, outputPath(pdfName + '.pdf'))
+
+def htmlPath(htmlName):
+	return './html/' + htmlName
+
+def outputPath(fileName):
+	return './finished/' + fileName
+
 def makeBarChart(chartData):	
 
 	n_groups = 10
@@ -123,18 +136,17 @@ def makeBarChart(chartData):
 	plt.legend()
 
 	plt.tight_layout()
-	savefig('html/quantifyPlot.png')
+	savefig(htmlPath("quantifyPlot.png"))
 	plt.clf()
 
-def makePieChart(fileName,chartData,chartTitle):
-	#zhfont1 = FontProperties(fname='C:\Windows\Fonts\arial.ttf')
-	x = ['1分', '2分', '3分', '4分', '5分', '6分']
+def makePieChart(fileName,chartData):
+	x = ['1', '2', '3', '4', '5', '6']
 	y = np.array(chartData)
 	colors = ['black','red','gold','lightskyblue','darkgreen','violet']
 	percent = 100.*y/y.sum()
 	explode = (0.05, 0.05, 0.05, 0.05, 0.05, 0.05)
 	plt.axis('equal')
-	patches, texts = plt.pie(y, colors=colors,explode=explode, startangle=90, radius=1)
+	patches = plt.pie(y, colors=colors,explode=explode, startangle=90, radius=1)
 
 	labels = [('{0} - {1:1.2f} %').format(i,j) for i,j in zip(x, percent)]
 
@@ -143,12 +155,14 @@ def makePieChart(fileName,chartData,chartTitle):
 	plt.clf()
 		
 def inputData():
-	global totalCopies,classData,className,docTitle
+	global totalCopies,classData,docTitle
 	print ("輸入社課日期:(ex:12月6號)")
 	classData[0] = rawInput()
 	print ("輸入講師名稱:")
 	classData[1] = rawInput()
 	docTitle =  classData[0] + classData[1] + '滿意度'
+	print("輸入社課教室:")
+	classData[2] = rawInput()
 
 
 	while True:
@@ -157,9 +171,10 @@ def inputData():
 		print ("輸入88來返回上一步")
 		print ("輸入99來完成報表")
 		print ("目前份數:" + str(totalCopies))
-		print ()
+
 		printData()
-		print('\n')
+		print()
+		print("     ↓↓↓↓↓↓↓↓↓↓↓")
 		data = rawInput()
 		appendData = data
 
@@ -190,16 +205,18 @@ def inputData():
 				data = data.split(' ')
 				if data[len(data)-1] is "":
 					data.remove("")
-				data = map(int,data)
+				data = list(map(int,data))
+				print(data)
 				print
 
 			except:
 				print ("輸入錯誤")
-				print
-				print
+				print("\n")
 				err = 1
+				clearScreen()
 				continue
 			check = 0
+			
 			try:
 				for num2 in range(0,len(data)):
 					question[num2][data[num2]-1] = question[num2][data[num2]-1]+1
@@ -209,7 +226,9 @@ def inputData():
 					question[num2][data[num2]-1] = question[num2][data[num2]-1]-1
 				print ("資料格式錯誤")
 				err = 1
+				clearScreen()
 				continue
+
 		if err != 1:
 			rollBack.append(appendData)
 			totalCopies = totalCopies + 1
@@ -217,16 +236,16 @@ def inputData():
 		clearScreen()
 	
 def finishData():
-	global adviseList
+	global adviseList, qualityAdvise, hasQualityAdvise
 	countAdvise = 0
 	
 	# using while will cause extra loop
 	while True:
-
-		print ("TotalInput: ")
+		clearScreen()
+		print ("已輸入內容: ")
 		for num in range(len(adviseList)):
 			print (adviseList[num])
-		print ("輸入88來回到上一步(""注意動作無法復原,絕對不是我懶得寫code :D"")")
+		print ("輸入88來復原")
 		print ("輸入99來完成")
 		print ("輸入心得及建議:")
 		inputAdvise = rawInput()
@@ -239,10 +258,22 @@ def finishData():
 		else:
 			print ("inputAdvise2:" + inputAdvise)
 			
+		
 		clearScreen()
+
 		countAdvise = countAdvise + 1
 		adviseList.append(addAdviseHeader[0] + str(countAdvise) + ". " + inputAdvise + addAdviseHeader[1])
-	
+
+	print("是否要輸入質化心得?(Y/N或其他值)")
+	ask = rawInput()
+	if ask is "Y" or ask is "y":
+		hasQualityAdvise = True
+		print("輸入心得:")
+		qualityAdvise = str(rawInput())
+	elif ask is "N" or ask is "n":
+		pass
+	clearScreen()
+
 def calculateQuantifyData():
 	quantifyData = []
 	for num in range(1,11):
@@ -256,27 +287,30 @@ def calculateQuantifyData():
 def generateDoc1(adviseList):
 	global docTitle
 	
-	adviseContent = readFile("./html/advise.html")
+	adviseContent = readFile(htmlPath("advise.html"))
 	
 	for num in range(len(adviseList)):
 		adviseContent.append(adviseList[num])
 	#或許可以用讀檔的方式避免錯誤roll back
 
-	adviseContent[adviseContent.index('@total')] = totalCopies
+	modifyKeyword('@total', adviseContent, totalCopies)
+	#adviseContent[adviseContent.index('@total')] = totalCopies
 	for num in range(4):
-		adviseContent[adviseContent.index('@g' + str(num))] = grade[num]
+		modifyKeyword('@g'+ str(num), adviseContent, grade[num])
+		#adviseContent[adviseContent.index('@g' + str(num))] = grade[num]
 	
 	for num in range(6):
 		for num2 in range(10):
-			adviseContent[adviseContent.index('@' + str(num2) + '-' + str(num))] = question[num2+1][num] #without grade
+			modifyKeyword('@' + str(num2) + '-' + str(num), adviseContent, question[num2+1][num])
+			#adviseContent[adviseContent.index('@' + str(num2) + '-' + str(num))] = question[num2+1][num] #without grade
 			
-	writeDataToHtml("./html/adviseTemp.html",adviseContent)
-	#convertHtmlToDoc("./html/adviseTemp.html",docTitle)
-	#os.remove("./html/adviseTemp.html")
+	writeDataToHtml(htmlPath("adviseTemp.html"),adviseContent)
+	convertHtmlToPDF(htmlPath("adviseTemp.html"), docTitle)
+	os.remove(htmlPath("adviseTemp.html"))
 	
 def generateDoc2():
-	quantifyContent = readFile("./html/quantify.html")
-	quantifyAdvise = readFile("./html/quantifyAdvise.txt")
+	quantifyContent = readFile(htmlPath("quantify.html"))
+	quantifyAdvise = readFile(htmlPath("quantifyAdvise.txt"))
 	quantifyData = calculateQuantifyData()
 	print ("making bar chart...")
 	makeBarChart(quantifyData)
@@ -291,50 +325,80 @@ def generateDoc2():
 			quantifyAdviseTemp = quantifyAdvise[num*3+1]
 		elif quantifyData[num] < 4.6:
 			quantifyAdviseTemp = quantifyAdvise[num*3+2]
-			
-		quantifyContent[quantifyContent.index("@" + str(num) + "-0" )] = quantifyData[num]
-		quantifyContent[quantifyContent.index("@" + str(num) + "-1" )] = round(quantifyData[num]/10,2)
-		quantifyContent[quantifyContent.index("@" + str(num) + "-2" )] = quantifyAdviseTemp
+
+		modifyKeyword("@info", quantifyContent, docTitle)
+
+		modifyKeyword("@" + str(num) + "-0", quantifyContent, quantifyData[num])
+		modifyKeyword("@" + str(num) + "-1", quantifyContent, "{:.2f}".format(round(quantifyData[num]/10,2)))
+		modifyKeyword("@" + str(num) + "-2", quantifyContent, quantifyAdviseTemp)
 		
-		quantifyTotalScore = quantifyTotalScore +  str(round(quantifyData[num]/10,2)) + "+"
-		quantifyTotalScoreSum = quantifyTotalScoreSum + round(quantifyData[num]/10,2)
-		
-		
-	quantifyContent[quantifyContent.index("@10-0")] = str(quantifyTotalScore[:-1]) + "=" + str(quantifyTotalScoreSum)
-	quantifyContent[quantifyContent.index("@10-1")] = str(quantifyTotalScoreSum) + "/6=" + str(round(quantifyTotalScoreSum/6,2)*100) + "%"
-	quantifyContent[quantifyContent.index("@10-2")] = docTitle
-	writeDataToHtml("./html/quantifyTemp.html",quantifyContent)
-	#convertHtmlToDoc("./html/quantifyTemp.html", docTitle + "quantify")
-	#os.remove("./html/quantifyTemp.html")
+		quantifyTotalScore += str(round(quantifyData[num]/10,2)) + "+"
+		quantifyTotalScoreSum += round(quantifyData[num]/10,2)
+
+	modifyKeyword("@10-0", quantifyContent, str(quantifyTotalScore[:-1]) + " = " + "{:.2f}".format(quantifyTotalScoreSum))
+	modifyKeyword("@10-1", quantifyContent, "{:.2f}".format(quantifyTotalScoreSum) + " /6=" + str("{:.2f}".format(round(quantifyTotalScoreSum/6,2)*100)) + "%")
+	modifyKeyword("@10-2", quantifyContent, docTitle)
+
+	writeDataToHtml(htmlPath("quantifyTemp.html"),quantifyContent)
+	convertHtmlToPDF(htmlPath("quantifyTemp.html"), docTitle + "量化")
+	os.remove(htmlPath("quantifyTemp.html"))
 
 def generateDoc3():
-	qualityContent = readFile("./html/quality.html")
-	qualityPieTitle = [u'1.整體評價',u'2. 事前宣傳',u'3. 場地',u'4. 講師上課流暢度',u'5. 講師的上課速度',u'6.講師表達清晰度',u'7. 主題和內容的契合度',u'8. 內容理解程度',u'9.主題和內容契合度',u'10.內容吸引程度']
+	qualityContent = readFile(htmlPath("quality.html"))
+	defaultAdvise = readFile(htmlPath("defaultQualityAdvise.txt"))[0]
+	#qualityPieTitle = ['1.整體評價','2. 事前宣傳','3. 場地','4. 講師上課流暢度','5. 講師的上課速度','6.講師表達清晰度','7. 主題和內容的契合度','8. 內容理解程度','9.主題和內容契合度','10.內容吸引程度']
 	
 	for num in range(10):
 		for num2 in range(6):
-			qualityContent[qualityContent.index('@' + str(num) + '-' + str(num2))] = question[num+1][num2]
+			modifyKeyword('@' + str(num) + '-' + str(num2), qualityContent, question[num+1][num2])
 			
-		makePieChart('html/@' + str(num) + '-6.png' , question[num+1], qualityPieTitle[num])
+		makePieChart(htmlPath('@' + str(num) + '-6.png'), question[num+1])
 		print ("make chart " + str(num))
-		
-	qualityContent[qualityContent.index('@10-1')] = totalCopies
-	qualityContent[qualityContent.index('@10-2')] = classData[0].encode('utf8')
-	qualityContent[qualityContent.index('@10-3')] = datetime.now().year-1911
-	qualityContent[qualityContent.index('@10-4')] = classData[1].encode('utf8')
-	qualityContent[qualityContent.index('@10-5')] = classData[1].encode('utf8')
+	
+	if hasQualityAdvise:
+		modifyKeyword("@10-0", qualityContent, qualityAdvise)
+	else:
+		modifyKeyword("@10-0", qualityContent, defaultAdvise)
+	modifyKeyword("@10-1", qualityContent, totalCopies)
+	modifyKeyword("@10-2", qualityContent, classData[0])
+	modifyKeyword("@10-3", qualityContent, datetime.now().year-1911)
+	modifyKeyword("@10-4", qualityContent, classData[1])
+	modifyKeyword("@10-5", qualityContent, classData[1])
+	modifyKeyword("@classroom", qualityContent, classData[2])
+
 	
 
-	writeDataToHtml("./html/qualityTemp.html",qualityContent)
-	#convertHtmlToDoc("./html/qualityTemp.html", docTitle + u'quality')
-	#os.remove("./html/qualityTemp.html")
-	
-inputData()
-finishData()
-print ("generating Doc1...")
-generateDoc1(adviseList)
-print ("generating Doc2...")
-generateDoc2()
-print ("generating Doc3...")
-generateDoc3()
-print ("complete")
+	writeDataToHtml(htmlPath("qualityTemp.html"),qualityContent)
+	convertHtmlToPDF(htmlPath("qualityTemp.html"), docTitle + "質化")
+	os.remove(htmlPath("qualityTemp.html"))
+
+def modifyKeyword(keyword,lists,modify):
+    for content in lists:
+        if keyword in content:
+            pos = lists.index(content)
+            lists[pos] = content.replace(keyword, str(modify))
+    return lists
+
+def main():
+	global ask
+	inputData()
+	finishData()
+	print ("generating Doc1...")
+	generateDoc1(adviseList)
+	print ("generating Doc2...")
+	generateDoc2()
+	print ("generating Doc3...")
+	generateDoc3()
+	print ("complete")
+	time.sleep(5)
+	print("是否填寫新的滿意度?(y/n)")
+	ask = rawInput()
+
+main()
+if ask is 'y':
+	main()
+	clearScreen()
+else:
+	print("app by brianchul @ tkucsie 20180607")
+	print("BYE")
+	time.sleep(5)
